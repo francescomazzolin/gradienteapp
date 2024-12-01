b0447c4d639868b62269e172a58e7335d5079ada
francescomazzolin <francesco.mazzolin11@gmail.com>
2024-12-01 16:21:59 +0100

    minorbug progressbar update

--- to_pager_functions_2.py
+++ to_pager_functions_2.py
@@ -397,7 +397,7 @@ App design functions
 """
 
 def update_progressbar(progress_bar, message_placeholder,
-                       milestone, step,
+                       milestone, steps,
                        message):
     progress_bar.progress(milestone / steps)
     message_placeholder.markdown(message)
