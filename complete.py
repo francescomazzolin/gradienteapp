7ca97820e97d9ddfe7287ca6ad7fe3bcddc626d8
francescomazzolin <francesco.mazzolin11@gmail.com>
2024-12-01 22:36:47 +0100

    commmit

--- complete.py
+++ complete.py
@@ -144,7 +144,7 @@ def chatbot_with_pdfs(default=True, pdf_docs=None):
     
     # Process the question
     if user_question and st.session_state.conversation:
-        with st.spinner("Fetching response..."):
+        with st.spinner("Fetching response...", key = 'bluespin'):
             time.sleep(40)
 
             try:
